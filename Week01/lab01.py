b73be88c7c0eb26aecb996a3eea7f2078acc0a83
samivsshanita@gmail.com <samiarrahman@Samis-MacBook-Air.local>
2025-01-12 21:07:55 -0500

    Second Commit

---
+++ Week01/lab01.py
@@ -0,0 +1,18 @@
+# Sample Coding Questions 01 Week01
+# Sami Ar Rahman
+
+array = [1,4,7,9]
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = (a - ((b ** c) // d)) + (a % c)
+
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+userAge = int(input("Enter your age: ")) + 22
+print("Now showing the shop items filtered by age: 22", userAge)
+
